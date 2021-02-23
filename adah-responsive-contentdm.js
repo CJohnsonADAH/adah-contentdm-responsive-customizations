04094f7952d6f935123e791584db24f60077e502
C. Johnson <charlesw.johnson@archives.alabama.gov>
2021-02-23 14:48:04 -0600

    Enhancement: Metadata Record Pages: display preview image of source cross-referenced by URL (ex.: 1867 voter registration page images). Step 1: extract the image and wrap it in a link. (#1)

--- adah-responsive-contentdm.js
+++ adah-responsive-contentdm.js
@@ -3,7 +3,7 @@
  * some of them original, some of them pasted in or adapted from the OCLC Cookbook, and some
  * adapted from the Ohio Memory CONTENTdm repository.
  *
- * @version 2021.0126
+ * @version 2021.0223-1442
  */
  
 /* video-stream-embed-1.2.js */
@@ -965,6 +965,243 @@ var adahCDMPageMenu = null;
 1.0 - 2019 July - initial implementation
 
 */
+
+/* -=-=-=-= pdf-objects-multipage-1_1.js =-=-=-=- */
+
+(function() {
+  'use strict';
+
+  function getFullPDFLink(collection, item) {
+    /**
+     * Get the full PDF URL if there is one.
+     * @param  {string} collection Alias of current collection.
+     * @param  {string} item ID # of current item.
+     * @return {object}         url: full PDF URL; page: current PDF page
+     */
+
+    return fetch('/digital/api/collections/' + collection + '/items/' + item + '/false')
+    .then(function(response) {
+      // return API info about item as JSON
+      return response.json();
+    })
+    .then(function(json) {
+      let detailsFullPDF = {};
+      console.log('has full pdf: ' + json.hasPrintPDF);
+      if (json.hasPrintPDF == true) {
+      // if full PDF exists, get the URL
+        detailsFullPDF.url = json.downloadParentUri;
+        if (item === json.parentId) {
+          //the current item is the parent item
+          detailsFullPDF.page = 0;
+        } else {
+          const itemNumber = Number(item);
+          //convert string to number for downstream equivalence
+          let pagePosition = json.parent.children.map(el => el.id).indexOf(itemNumber);
+          pagePosition++;
+          //add one to align value to pdf.js #page= parameter
+          detailsFullPDF.page = pagePosition;
+        }
+        return detailsFullPDF;
+        //return is an object
+      } else {
+        return false;
+      }
+    })
+    .catch(function(error) {
+      console.log('No full PDF link found: ' + error);
+    })
+  }
+
+  function lightboxCleanup() {
+    let lightbox = document.getElementById('full-pdf-viewer');
+    lightbox.parentNode.removeChild(lightbox);
+  }
+
+  function buildLightbox(link,page) {
+    /**
+     * Get the full PDF URL if there is one.
+     * @param  {string} link The URL of the full PDF.
+     * @param  {string} page The specific page of the PDF currently viewed.
+     */
+
+    const previousLightbox = document.getElementById('full-pdf-viewer');
+    if (previousLightbox) {
+      //cleanup any previous instances of lightbox
+      previousLightbox.parentNode.removeChild(previousLightbox);
+    }
+    const newLightbox = document.createElement('div');
+      newLightbox.className = 'Lightbox-lightbox';
+      newLightbox.id = 'full-pdf-viewer';
+    const itemContainer = document.createElement('div');
+      itemContainer.id = 'full-pdf-container';
+      itemContainer.className = 'ItemModalViewer-container';
+    const pdfContainer = document.createElement('div');
+      pdfContainer.className = 'PDFViewer-pdfFrame';
+    const iframePDF = document.createElement('iframe');
+      let fullPDFLink = link;
+      let currentPage = Number(page);
+      if (currentPage === 0) {
+      //if parent is being viewed, render page 1 in pdf.js
+        fullPDFLink += '#page=1';
+      } else if (currentPage > 0) {
+        fullPDFLink += '#page=' + page;
+      }
+      iframePDF.src = '/digital/pdf.js/web/viewer.html?file=' + fullPDFLink;
+    const closeButtonContainer = document.createElement('div');
+      closeButtonContainer.className = 'btn-group';
+    const closeButton = document.createElement('button');
+    const closeIcon = document.createElement('i');
+      closeIcon.classList.add('fa','fa-times-circle');
+    closeButton.appendChild(closeIcon);
+    closeButton.addEventListener('click', function(){lightboxCleanup();});
+    closeButtonContainer.appendChild(closeButton);
+
+    pdfContainer.appendChild(iframePDF);
+    itemContainer.appendChild(pdfContainer);
+    itemContainer.appendChild(closeButtonContainer);
+    newLightbox.appendChild(itemContainer);
+
+    document.querySelector('[data-id="itemViewPageFound"]').prepend(newLightbox);
+  }
+
+  function changeButtonBehavior(detailsPDF)  {
+    let button = document.querySelector('button.ItemPDF-expandButton');
+    button.addEventListener('click', function(e){
+      e.stopPropagation();
+      buildLightbox(detailsPDF.url,detailsPDF.page);
+    });
+  }
+
+  function replaceLightbox(collection, item) {
+  //helper function to sequence fetch promises
+    getFullPDFLink(collection, item)
+    .then(function(response) {
+      if (response) {
+        changeButtonBehavior(response);
+      }
+    });
+  }
+
+  let globalScope = true;
+  let collectionScope = [
+    ''
+  ];
+
+  document.addEventListener('cdm-item-page:ready', function(e) {
+    const collection = e.detail.collectionId;
+    if (globalScope || collectionScope.includes(collection)) {
+      replaceLightbox(collection, e.detail.itemId);
+    }
+  });
+
+  document.addEventListener('cdm-item-page:update', function(e) {
+    const collection = e.detail.collectionId;
+    if (globalScope || collectionScope.includes(collection)) {
+      replaceLightbox(collection, e.detail.itemId);
+    }
+  });
+
+  document.addEventListener('cdm-item-page:leave', function(e) {
+    const collection = e.detail.collectionId;
+    if (globalScope || collectionScope.includes(collection)) {
+      //nothing to clean up at page exit
+    }
+  });
+
+})();
+
+/* version history
+
+1.1 - 2020 Sep 29 - a few bug fixes; added code comments
+1.0 - 2020 Sep 28 - initial implementation
+
+*/
+
+(function() {
+  'use strict';
+
+	let crossReferenceScanObjects = [
+		'cdm-item-page',
+	];
+	let crossReferenceScanEvents = [
+		'ready',
+		'update'
+	];
+		
+	for (var i = 0; i < crossReferenceScanObjects.length; i++) {
+		for (var j = 0; j < crossReferenceScanEvents.length; j++) {
+			// custom menus
+			let crossReferenceScanEvent = crossReferenceScanObjects[i] + ':' + crossReferenceScanEvents[j];
+			document.addEventListener(crossReferenceScanEvent, adahInsertCrossReferencedImageLink);
+
+		} /* for */
+	} /* for */
+
+function adahInsertCrossReferencedImageLink () {
+	let el = getCrossReferencedImageLink();
+	if ( el ) {
+		console.log("in adahInsertCrossReferencedImageLink", "got cross-referenced image:", el);
+		let preview = document.getElementsByClassName('preview');
+		for (let i = 0; i<preview.length; i++) {
+			let children = preview[i].getElementsByTagName('img');
+			if ( children.length == 0 ) {
+				console.log("in adahInsertCrossReferencedImageLink", "append child:", el);
+				preview[i].appendChild(el);
+			}
+		}
+	}
+	
+}
+
+function getCrossReferencedImageLink ( imgSlug ) {
+	let elLinkedImg = null;
+	let elImg = getCrossReferenceLinkImage(imgSlug);
+
+	if ( elImg ) {
+		elLinkedImg = document.createElement('a');
+		elLinkedImg.appendChild(elImg);
+		if ( elImg.getAttribute('data-link') ) {
+			elLinkedImg.href = elImg.getAttribute('data-link');
+		}
+	}
+	return elLinkedImg;
+	
+}
+
+function getCrossReferenceLinkImage ( imgSlug ) {
+
+	let elDescriptionTable = document.getElementById('singleItemDescription');
+	
+	let elImg = null;
+
+	if ( ! imgSlug ) { 
+		imgSlug = 'default.jpg?highlightTerms=';
+	}
+	
+	if ( elDescriptionTable ) {
+		let elKeyValuePair = elDescriptionTable.getElementsByClassName('field-descri');
+		if ( elKeyValuePair.length > 0 ) {
+			let elValue = elKeyValuePair[0].getElementsByClassName('field-value');
+			if ( elValue.length > 0 ) {
+				let elValueLink = elValue[0].getElementsByTagName('a');
+				if ( elValueLink.length > 0 ) {
+					elImg = document.createElement('img');
+					
+					let sHref = elValueLink[0].href;
+					sHref = sHref.replace(/(\/digital\/)collection\/([^/]+\/)id\/([^/]+)(\/.*)?$/, "$1api/singleitem/image/$2$3");
+					
+					elImg.src = ( sHref + "/" + imgSlug );
+					elImg.alt = elValue[0].innerText;
+					elImg.setAttribute( 'data-link',  elValueLink[0].href  );
+				}
+			}
+		}
+	}
+	
+	return elImg;
+}
+
+})();
 
 function ScriptLoader(url, callback){
 	console.log("ScriptLoader:", url, callback);
